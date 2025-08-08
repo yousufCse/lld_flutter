70d04fb2cb12ba84745109e62f83d236fbd1103c
Md. Yousuf Ali <myali@Yousufs-MacBook-Pro.local>
2025-08-08 14:42:06 +0600

    chore: excercise chapter 01 from refactoring book

---
+++ examples/code_refactoring/refactoring_martin_fowler/chapter_01/function_statement.dart
@@ -0,0 +1,68 @@
+// ignore_for_file: avoid_print
+
+import 'dart:math';
+
+String statement(Map<String, dynamic> invoice, Map<String, dynamic> plays) {
+  var totalAmount = 0.0;
+  var volumeCredits = 0;
+  var result = 'Statement for ${invoice['customer']}\n';
+
+  for (var perf in invoice['performances']) {
+    var play = plays[perf['playID']];
+    var thisAmount = 0.0;
+
+    switch (play['type']) {
+      case 'tragedy':
+        thisAmount = 40000;
+        if (perf['audience'] > 30) {
+          thisAmount += 1000 * (perf['audience'] - 30);
+        }
+        break;
+      case 'comedy':
+        thisAmount = 30000;
+        if (perf['audience'] > 20) {
+          thisAmount += 10000 + 500 * (perf['audience'] - 20);
+        }
+        thisAmount += 300 * perf['audience'];
+        break;
+      default:
+        throw Exception('Unknown type: ${play['type']}');
+    }
+
+    // add volume credits
+    volumeCredits += max((perf['audience'] as int) - 30, 0);
+    // volumeCredits += perf['audience'] as int;
+    if (play['type'] == 'comedy') {
+      volumeCredits += (perf['audience'] as int) ~/ 5;
+    }
+
+    // print line for this order
+    result +=
+        ' ${play['name']}: \$${(thisAmount / 100).toStringAsFixed(2)} (${perf['audience']} seats)\n';
+    totalAmount += thisAmount;
+  }
+
+  result += 'Amount owed is \$${(totalAmount / 100).toStringAsFixed(2)}\n';
+  result += 'You earned $volumeCredits credits\n';
+  return result;
+}
+
+// performance['as-like'] = {'name': 'As You Like It', 'type': 'comedy'};
+void main(List<String> args) {
+  var invoice = {
+    'customer': 'BigCo',
+    'performances': [
+      {'playID': 'hamlet', 'audience': 55},
+      {'playID': 'as-like', 'audience': 35},
+      {'playID': 'othello', 'audience': 40},
+    ],
+  };
+
+  var plays = {
+    'hamlet': {'name': 'Hamlet', 'type': 'tragedy'},
+    'as-like': {'name': 'As You Like It', 'type': 'comedy'},
+    'othello': {'name': 'Othello', 'type': 'tragedy'},
+  };
+
+  print(statement(invoice, plays));
+}
