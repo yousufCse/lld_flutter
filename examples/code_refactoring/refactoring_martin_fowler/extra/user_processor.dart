d2703fefe282a5337de58b34c28a0959b0fc891c
Md. Yousuf Ali <myali@Yousufs-MacBook-Pro.local>
2025-08-13 21:02:08 +0600

    refactor: rename variables

--- examples/code_refactoring/refactoring_martin_fowler/extra/user_processor.dart
+++ examples/code_refactoring/refactoring_martin_fowler/extra/user_processor.dart
@@ -3,67 +3,71 @@
 import 'dart:math';
 
 class UserProcessor {
-  dynamic processUserData(var d, var opts, bool isVIP) {
+  dynamic processUserData(
+    Map<String, dynamic> data,
+    Map<String, dynamic> options,
+    bool isVip,
+  ) {
     // Initialize variables
-    var tot = 0.0;
-    var dis = 0.0;
-    var res = "";
-    var user_products = [];
-    var user_name = d["name"];
+    var total = 0.0;
+    var discount = 0.0;
+    var result = "";
+    var products = [];
+    var userName = data["name"];
 
     // Calculate total price of all products
-    for (var i = 0; i < d["products"].length; i++) {
-      var p = d["products"][i];
-      tot = tot + p["price"] * p["quantity"];
-      user_products.add(p["name"]);
+    for (var i = 0; i < data["products"].length; i++) {
+      var product = data["products"][i];
+      total = total + product["price"] * product["quantity"];
+      products.add(product["name"]);
     }
 
     // Apply different discount based on user type
-    if (isVIP == true) {
-      if (tot > 1000) {
-        dis = tot * 0.15;
-      } else if (tot > 500) {
-        dis = tot * 0.1;
+    if (isVip == true) {
+      if (total > 1000) {
+        discount = total * 0.15;
+      } else if (total > 500) {
+        discount = total * 0.1;
       } else {
-        dis = tot * 0.05;
+        discount = total * 0.05;
       }
     } else {
-      if (tot > 1000) {
-        dis = tot * 0.1;
-      } else if (tot > 500) {
-        dis = tot * 0.05;
+      if (total > 1000) {
+        discount = total * 0.1;
+      } else if (total > 500) {
+        discount = total * 0.05;
       } else {
-        dis = 0;
+        discount = 0;
       }
     }
 
     // Apply special discount if specified in options
-    if (opts != null && opts["specialDiscount"] == true) {
-      dis = dis + 50;
+    if (options["specialDiscount"] == true) {
+      discount = discount + 50;
     }
 
     // Make sure discount doesn't exceed total
-    if (dis > tot) {
-      dis = tot;
+    if (discount > total) {
+      discount = total;
     }
 
     // Generate report
-    res = "User: " + user_name + "\n";
-    res = res + "Products: " + user_products.join(", ") + "\n";
-    res = res + "Subtotal: " + tot.toStringAsFixed(2) + "\n";
-    res = res + "Discount: " + dis.toStringAsFixed(2) + "\n";
-    res = res + "Total: " + (tot - dis).toStringAsFixed(2);
+    result = "User: " + userName + "\n";
+    result = result + "Products: " + products.join(", ") + "\n";
+    result = result + "Subtotal: " + total.toStringAsFixed(2) + "\n";
+    result = result + "Discount: " + discount.toStringAsFixed(2) + "\n";
+    result = result + "Total: " + (total - discount).toStringAsFixed(2);
 
     // Generate random order ID
     var order_id = "ORD" + (Random().nextInt(9000) + 1000).toString();
 
     // Return final data
-    var finalData = {
-      "report": res,
+    final finalData = {
+      "report": result,
       "order_id": order_id,
-      "total": tot,
-      "discount": dis,
-      "final_amount": tot - dis,
+      "total": total,
+      "discount": discount,
+      "final_amount": total - discount,
       "processed_on": DateTime.now().toString(),
     };
 
