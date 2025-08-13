5051e7f5709216ad0281005e2a395fa368034526
Md. Yousuf Ali <myali@Yousufs-MacBook-Pro.local>
2025-08-13 20:52:08 +0600

    test: add test for user processor

---
+++ examples/code_refactoring/refactoring_martin_fowler/extra/test/user_processor_test.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: avoid_print
+
+import 'package:flutter_test/flutter_test.dart';
+
+import '../user_processor.dart';
+
+void main() {
+  var userData = {
+    "name": "John Smith",
+    "products": [
+      {"name": "Laptop", "price": 1200, "quantity": 1},
+      {"name": "Mouse", "price": 25, "quantity": 2},
+      {"name": "Keyboard", "price": 50, "quantity": 1},
+    ],
+  };
+
+  var options = {"specialDiscount": true};
+
+  var processor = UserProcessor();
+  var result = processor.processUserData(userData, options, true);
+  print(result["report"]);
+  final report = result["report"];
+
+  test('Function statement generates correct output for given info', () {
+    expect(report, contains('User: John Smith'));
+    expect(report, contains('Products: Laptop, Mouse, Keyboard'));
+    expect(report, contains('Subtotal: 1300.00'));
+    expect(report, contains('Discount: 245.00'));
+    expect(report, contains('Total: 1055.00'));
+    expect(result["order_id"], isNotNull);
+    expect(result["total"], 1300.00);
+    expect(result["discount"], 245.00);
+    expect(result["final_amount"], 1055.00);
+    expect(result["processed_on"], isNotNull);
+  });
+}
